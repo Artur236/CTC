d1b4d42106a9e6a547c4f6a0b2f40b1e75e2b4f9
Arthur Poliakov <poliakov.arthur@gmail.com>
2016-07-18 16:32:28 +0300

    layouts without responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function(){
 	$(window).scroll(function() {
-		if ($(window).scrollTop() > 400) {
+		if ($(window).scrollTop() > 350) {
 			$('.header .nav_bar').addClass('navbar-fixed-top');
 		}
 		else {
@@ -18,14 +18,13 @@ $(function(){
 	function progressSim() {
 		diff = al / 100 * Math.PI * 2;
 		ctx.clearRect(0, 0, cw, ch);
-		ctx.lineWidth = 20;
+		ctx.lineWidth = 5;
 		ctx.fillStyle = '#fff';
 		ctx.strokeStyle = gradient;
-		gradient.addColorStop("0.1","darkblue");
-		gradient.addColorStop("1","blue");
-		gradient.addColorStop("0.5","cyan");
+		gradient.addColorStop("0.1","#5acd79");
+		gradient.addColorStop("1","#fdc21d");
 		ctx.textAlign = 'center';
-		ctx.font = "20px Arial";
+		ctx.font = "lighter 40px Arial";
 		ctx.fillText(al + '%',  cw * 0.5, ch * 0.5 + 2, cw);
 		ctx.beginPath(); 
 		ctx.arc(100, 100, 70, start, diff + start, false);
@@ -48,5 +47,17 @@ $(function(){
 			}
 			);
 		});
+		$('img').retina({
+    // Check for data-retina attribute. If exists, swap out image
+    dataRetina: true,
+     // Suffix to append to image file name
+     suffix: "",
+     // Check if image exists before swapping out
+     checkIfImageExists: false,
+     // Callback function if custom logic needs to be applied to image file name
+     customFileNameCallback: "",
+     // override window.devicePixelRatio
+     overridePixelRation: false
+ });
 	});
 
